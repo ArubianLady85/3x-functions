90151ab987dc2df63b0904e1e510f239f5db0082
ArubianLady85 <88424294+ArubianLady85@users.noreply.github.com>
2021-11-28 22:49:53 +0100

    Add files via upload

---
+++ exercise 8/functions3.js
@@ -0,0 +1,19 @@
+// old style
+function shopper(number1, number2) {
+    console.log(number1 * number1 + number2 * number2);
+}
+
+shopper(8, 15);
+
+//expression
+const tas = function (no1, no2) {
+    console.log(no1 * no1 + no2 * no2);
+};
+
+tas(8, 15);
+
+//arrows
+const blij = (number1, number2) => {
+    console.log(number1 * number1 + number2 * number2);
+};
+blij(8, 15);
